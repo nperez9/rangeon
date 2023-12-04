aba6202fef49e25573d0f77b27073025eabf3f83
Nicolas Perez <nicolasperez95.a@gmail.com>
2023-12-03 22:25:07 -0300

    feat: added cool stuff

---
+++ Assets/Scripts/Constants/Layers.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Layers
+{
+    public const string WALL = "Wall";
+    public const string ENEMY = "Enemy";
+    public const string OBJECT = "Object";
+    public const string PLAYER = "Player";
+}
